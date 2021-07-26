88e3db244602881b70ef77b9432e531215d7ad75
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-07-26 16:04:19 +0530

    refactor: notification query updated

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -4,10 +4,12 @@ const getUserNotifications = async (req, res, next) => {
   try {
     const { userId } = req;
     const { since } = req.query;
-    const notifications = await Notification.find({ destinationUser: userId })
+    const notifications = await Notification.find({
+      destinationUser: userId,
+    })
       .where("createdAt")
       .gt(new Date(since))
-      .sort({ createdAt: "desc" })
+      .sort("-createdAt")
       .select("-__v");
     res.status(200).json({ success: true, notifications });
   } catch (error) {
